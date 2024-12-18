3cb31222c8dc54759a679810db02dd1b349d41ca
Novakboh-step <novak_dv82@student.itstep.org>
2024-12-18 10:51:34 +0200

    Added messagebox in case of error

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from random import randint
 
 def generate():
-    label1.config(text=randint(int(cb1.get()), int(cb2.get())))
+    fr = int(cb1.get())
+    to = int(cb2.get())
+    if fr >= to:
+        messagebox.showinfo('Error', 'Wrong input')
+    else:
+        label1.config(text=randint(fr, to))
 
 root = Tk()
 root.geometry("200x100")
 root.title("Random number")
 label1 = Label(text=0)
-label1.pack()
-cb1 = ttk.Combobox(values=[-200, -100, 0, 100, 200])
-cb1.pack()
-cb2 = ttk.Combobox(values=[-200, -100, 0, 100, 200])
-cb2.pack()
+label1.grid(row=0, column=1)
+cblabel1 = Label(text="From:")
+cblabel1.grid(row=1, column=0)
+cb1 = ttk.Combobox(values=[-200, -100, 0, 100])
+cb1.grid(row=1, column=1)
+cblabel1 = Label(text="To:")
+cblabel1.grid(row=2, column=0)
+cb2 = ttk.Combobox(values=[-100, 0, 100, 200])
+cb2.grid(row=2, column=1)
 button = Button(text="Generate", command=generate)
-button.pack()
+button.grid(row=3, column=1)
 root.mainloop()
